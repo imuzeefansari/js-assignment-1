950c34efb4ad946f6bdbed481e1be50de84c5462
imuzeefansari <imuzeefansari@gmail.com>
2023-04-09 11:35:55 +0500

    Adding all files

---
+++ app.js
@@ -0,0 +1,95 @@
+//  (1)
+
+// let num1 = +prompt("Enter First Number");
+// let num2 = +prompt("Enter Second Number");
+// let largest;
+
+// if(num1 >= num2){
+//     largest = num1
+// }else{
+//     largest = num2
+// }
+// console.log('The Largest Number is ' + largest);
+
+// (2)
+
+// let num = +prompt("Write a Number to find a sign");
+
+// if(num >0){
+//     alert("The Sign is + " )
+// }else{
+//     alert("The Sign is - ")
+// }
+
+// (3)
+
+// let num1 = +prompt("Enter First Number");
+// let num2 = +prompt("Enter Second Number");
+// let num3 = +prompt("Enter Third Number");
+// let num4 = +prompt("Enter Fourth Number");
+// let num5 = +prompt("Enter fifth Number");
+
+// if(num1 >= num2 && num1 >= num3 && num1 >= num4 && num1 >= num5){
+//     largest = num1;
+// }else if(num2 >= num3 && num2 >= num1 && num2 >= num4 && num1 >= num5){
+//     largest = num2;
+// }else if (num3 >= num1 && num3 >= num2 && num3 >= num4 && num1 >= num5){
+//     largest = num3;
+// }else if(num4 >= num1 && num4 >= num2 && num4 >= num3 && num4 >= num5){
+//   largest = num4;
+// }else{
+//     largest = num5;
+// }
+
+// alert('The largest number is ' + largest);
+
+// (4)
+
+
+// for (let i = 0; i <= 15; i++) {
+//     if(i % 2 === 0){
+//   alert(i + " This is an Even Number");
+// }else{
+//   alert(i + " This is an Odd Number")
+// }
+// }
+
+// (5)
+
+// let percentage = +prompt("Write your number");
+
+// if(percentage >= 90){
+//  alert("Perfect ! your grade is A");
+// }else if (percentage >= 80){
+//   alert("your grade is B");
+// }else if (percentage >= 70){
+//   alert("your grade is C");
+// }else if (percentage >= 60){
+//   alert("your grade is D");
+// }else{
+//   alert("Your Grade is F");
+// }
+
+// (6)
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+//     if(i % 3 === 0 && i % 5 === 0){
+//       console.log("Fizz Buzz");
+//     }
+//   else if(i % 3 === 0){
+//         console.log("Fizz");
+//     }else if (i % 5 === 0){
+//         console.log("Buzz");
+//     }
+// }
+
+// (7)
+
+// for(let i = 1; i <=5; i++){
+//    let star = "";
+//    for (let j = 1; j <=i; j++){
+//      star = star + "* ";
+//    }
+//    console.log(star);
+// }
